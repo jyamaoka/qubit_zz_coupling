e369846dd859c2f49b39bab699968e92a351a03f
Jared Yamaoka <jared.yamaoka@outlook.com>
2025-06-27 16:49:09 -0400

    fix test

--- tests/test_core.py
+++ tests/test_core.py
@@ -7,22 +7,23 @@ import numpy as np
 @pytest.fixture
 def system_params():
     return {
-        "fq1": 3.2,  # GHz, Qubit 1 frequency (3.2)
-        "fq2": 3.35,  # GHz, Qubit 2 frequency
-        "fTLS": 3.2,  # GHz, TLS frequency (resonant with Qubit 1)
-        "JTLS": 0.5,  # GHz, Coupling strength between Qubit 1 and TLS
-        "relaxation": {"q1": 1/200, "q2": 1/250, "TLS": 1/500},  # Relaxation
-        "dephasing": {"q1": 1/150, "q2": 1/180, "TLS": 1/300},  # Dephasing
-        "Jzz": 0.00,  # Jzz coupling
-        "Jxx": 0.00,  # Jxx coupling
-        "Jxttls": 0.00,  # Jxt coupling
-        "Jxtqq": 0.00,  # Jxt coupling
-        "time_points": np.linspace(0, 100, 55)  # Time points in μs
+        "f_q1": 3.2,  # GHz, Qubit 1 frequency (3.2)
+        "f_q2": 3.35,  # GHz, Qubit 2 frequency
+        "f_tls": 3.2,  # GHz, TLS frequency (resonant with Qubit 1)
+        "T1": {"q1": 20000, "q2": 25000, "tls": 1e8},  # (ns) Relaxation rates
+        "T2": {"q1": 15000, "q2": 18000, "tls": 1e8},  # Dephasing rates
+        "J_zz": 0.01, # Jzz coupling
+        "J_tls": 0.01,  # GHz, Coupling strength between Qubit 1 and TLS
+        "omega1": 2*np.pi * 0.01, # drive amplitude (sets π/2 duration)
+        "omega2": 2*np.pi * 0.01, # drive amplitude (sets π/2 duration)
+        "time_points": np.linspace(0, 300, 105),  # Time points in μs
+        "ver1": True,
+        "single": False
     }
 
 
 def test_solve_t1(system_params):
-    H, c_ops, sz_q1, sz_q2, sx_q1, sx_q2 = setup_operators(system_params)
+    H, c_ops, sz_q1, sz_q2, sx_q1, sx_q2, sm_q1, sm_q2 = setup_operators(system_params)
     tlist = system_params["time_points"]
     psi0 = tensor(basis(2, 0), basis(2, 1), basis(2, 1))
 
@@ -33,10 +34,10 @@ def test_solve_t1(system_params):
 
 
 def test_solve_t2(system_params):
-    H, c_ops, sz_q1, sz_q2, sx_q1, sx_q2 = setup_operators(system_params)
+    H, c_ops, sz_q1, sz_q2, sx_q1, sx_q2, sm_q1, sm_q2 = setup_operators(system_params)
     tlist = system_params["time_points"]
-    fq1 = system_params["fq1"]
-    df1 = system_params["dephasing"]["q1"]
+    fq1 = system_params["f_q1"]
+    df1 = system_params["T2"]["q1"]
     psi0 = tensor((basis(2, 0) + basis(2, 1)).unit(), basis(2, 1), basis(2, 1))
 
     fit_params = solve_t2(H, psi0, tlist, c_ops, e_ops=[sx_q1], first_guess=[
@@ -47,7 +48,7 @@ def test_solve_t2(system_params):
 
 
 def test_setup_operators(system_params):
-    H, c_ops, sz_q1, sz_q2, sx_q1, sx_q2 = setup_operators(system_params)
+    H, c_ops, sz_q1, sz_q2, sx_q1, sx_q2, sm_q1, sm_q2 = setup_operators(system_params)
 
     assert H is not None
     assert len(c_ops) == 6  # Number of collapse operators
